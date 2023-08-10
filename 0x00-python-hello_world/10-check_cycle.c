179f78fca7809a98c5440bf5642333e92a257e55
Jonathan <jonathankyule2@gmail.com>
2023-08-10 18:38:42 +0000

    checking cycles correction

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
  * check_cycle - checks if a list contains a cycle
@@ -8,21 +9,26 @@
 
 int check_cycle(listint_t *list)
 {
-	listint_t *slow = list;
-	listint_t *fast = list;
+	listint_t *slow;
+	listint_t *fast;
 
-	if (!list)
+	if (list == NULL || list->next == NULL)
 	{
 		return (0);
 	}
-	while (slow && fast->next)
+
+	slow = list->next;
+	fast = list->next->next;
+
+	while (slow && fast && fast->next)
 	{
-		slow = slow->next;
-		fast = fast->next->next;
 		if (slow == fast)
 		{
 			return (1);
 		}
+
+		slow = slow->next;
+		fast = fast->next->next;
 	}
 	return (0);
 }
