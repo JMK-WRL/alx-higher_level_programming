7a664f1cd48a59f017cc61a36bfd28ec141095f4
Jonathan <jonathankyule2@gmail.com>
2023-08-12 12:12:10 +0000

    arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    num_args = len(sys.argv) - 1
+    if num_args == 0:
+        print("Number of arguments: 0.")
+        print(".")
+    else:
+        print("Number of arguments: {}:".format(num_args))
+
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
+        print()
