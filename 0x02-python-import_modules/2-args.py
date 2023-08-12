40702c88e939e459a068036444199739aac862b4
Jonathan <jonathankyule2@gmail.com>
2023-08-12 12:20:44 +0000

    corrections

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,10 +5,10 @@ if __name__ == "__main__":
 
     num_args = len(sys.argv) - 1
     if num_args == 0:
-        print("arguments: 0.")
+        print("0 arguments")
         print(".")
     else:
-        print("arguments: {}:".format(num_args))
+        print("arguments {}:".format(num_args))
 
         for i in range(1, len(sys.argv)):
             print("{}: {}".format(i, sys.argv[i]))
