38b28847932f70f5f83851b6aa43fc55d5eb5c6f
Jonathan <jonathankyule2@gmail.com>
2023-08-15 01:32:34 -0700

    task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
 def not_c(my_string):
-    new_string =""
-    for char in my_string:
-        if char != 'c'and char != 'C':
-            new_string += char
-    return new_string
+    rem = [x for x in my_string if x !='c' and x != 'C']
+    return ("".join(copy))
