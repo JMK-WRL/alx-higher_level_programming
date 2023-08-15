d66f458edab51e9e8686d58d63d8b09c7fc20693
Jonathan <jonathankyule2@gmail.com>
2023-08-14 19:47:42 -0700

    multiple of 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,8 +3,8 @@
 def divisible_by_2(my_list=[]):
     result_list = []
 
-    for num in my_list:
-        if num % 2 == 0:
+    for num in range(len(my_list)):
+        if my_list[num] % 2 == 0:
             result_list.append(True)
         else:
             result_list.append(False)
