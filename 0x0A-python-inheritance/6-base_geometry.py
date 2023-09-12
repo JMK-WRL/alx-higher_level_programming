82c8d57e27fd31123dfdcea2a786a88523071aca
Jonathan Kyule <121217004+JMK-WRL@users.noreply.github.com>
2023-09-12 03:10:10 +0000

    Improve geometry task 6
    Signed-off-by: Jonathan Kyule <121217004+JMK-WRL@users.noreply.github.com>

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Class of module BaseGeometry"""
+
+
+class BaseGeometry:
+    def area(self):
+        """
+        Raise an Exception with the message "area() is not implemented."
+
+        Raises:
+            Exception: Always raises an exception with the specified message.
+        """
+        raise Exception("area() is not implemented")
