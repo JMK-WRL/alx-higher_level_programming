5972ec715b1a4388604b203893feed8cfd100bc3
Jonathan <jonathankyule2@gmail.com>
2023-09-12 11:02:06 -0700

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,7 +4,7 @@
 
 def write_file(filename="", text=""):
     """
-    Write a string to a text file (UTF8) and return the number of characters written.
+    Write a string to a text file (UTF8)
 
     :param filename: The name of the file to write to.
     :param text: The text to write to the file.
@@ -13,6 +13,7 @@ def write_file(filename="", text=""):
     with open(filename, 'w', encoding='utf-8') as file:
         nb_characters = file.write(text)
     return nb_characters
+
 
 if __name__ == "__main__":
     nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,7 +4,7 @@
 
 def append_write(filename="", text=""):
     """
-    Append a string to the end of a text file (UTF8) and return the number of characters added.
+    Append a string to the end of a text file (UTF8)
 
     :param filename: The name of the file to append to.
     :param text: The text to append to the file.
@@ -13,6 +13,7 @@ def append_write(filename="", text=""):
     with open(filename, 'a', encoding='utf-8') as file:
         nb_characters_added = file.write(text)
     return nb_characters_added
+
 
 if __name__ == "__main__":
     nb_characters_added = append_write("file_append.txt", "This School is so cool!\n")
