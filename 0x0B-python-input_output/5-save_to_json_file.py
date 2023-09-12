64bb45e5c4e62692828eb070d991217201977c5d
Jonathan Kyule <121217004+JMK-WRL@users.noreply.github.com>
2023-09-12 18:21:05 +0000

    task 5 correction
    Signed-off-by: Jonathan Kyule <121217004+JMK-WRL@users.noreply.github.com>

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,37 +1,14 @@
 #!/usr/bin/python3
+"""
+writes an object to the text file using JSON
+"""
 import json
+
 
 def save_to_json_file(my_obj, filename):
     """
-    Write a Python object to a text file using its JSON representation.
-
-    :param my_obj: The Python object to be saved to the file.
-    :param filename: The name of the JSON file to write to.
+    writes object to text files
     """
-    with open(filename, 'w', encoding='utf-8') as file:
-        json.dump(my_obj, file)
-
-if __name__ == "__main__":
-    filename = "my_list.json"
-    my_list = [1, 2, 3]
-    save_to_json_file(my_list, filename)
-
-    filename = "my_dict.json"
-    my_dict = { 
-        'id': 12,
-        'name': "John",
-        'places': [ "San Francisco", "Tokyo" ],
-        'is_active': True,
-        'info': {
-            'age': 36,
-            'average': 3.14
-        }
-    }
-    save_to_json_file(my_dict, filename)
-
-    try:
-        filename = "my_set.json"
-        my_set = { 132, 3 }
-        save_to_json_file(my_set, filename)
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
+    with open(filename, 'w', encoding='utf-8') as f:
+        cont = json.dumps(my_obj)
+        f.write(cont)
