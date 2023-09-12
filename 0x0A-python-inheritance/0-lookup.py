bc69f8d55369df78aedaa54c6efa4c273c3c537a
Jonathan Kyule <121217004+JMK-WRL@users.noreply.github.com>
2023-09-12 01:57:49 +0000

    Lookup task 1
    Signed-off-by: Jonathan Kyule <121217004+JMK-WRL@users.noreply.github.com>

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""class define lookup """
+
+def lookup(obj):
+    """
+    This function takes an objext as input and returns a list of its attributes
+
+    Args:
+        obj(object): the object to inspect
+    """
+    return dir(obj)
