b1cd64247fc4bfcc37b268895ed0c20e989ee6e4
Jonathan Kyule <121217004+JMK-WRL@users.noreply.github.com>
2023-09-12 02:52:00 +0000

    exact same object task 2
    Signed-off-by: Jonathan Kyule <121217004+JMK-WRL@users.noreply.github.com>

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def is_same_class(obj, a_class):
+    """
+    check if an object is exactly an instant of the specified class
+
+    Args:
+        abj (object): the object to check
+        a_class: the class to compare against
+    """
+    return type(obj) is a_class
