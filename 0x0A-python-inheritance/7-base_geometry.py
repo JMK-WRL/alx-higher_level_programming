3d7935072b885ac967399692206bec5b95fc3b75
Jonathan Kyule <121217004+JMK-WRL@users.noreply.github.com>
2023-09-14 01:44:13 +0000

    task 7 correction
    Signed-off-by: Jonathan Kyule <121217004+JMK-WRL@users.noreply.github.com>

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -7,9 +7,7 @@ class BaseGeometry:
     def area(self):
         """
         Raise an Exception with the message "area() is not implemented."
-
-        Raises:
-            Exception: Always raises an exception with the specified message.
+        
         """
         raise Exception("area() is not implemented")
 
@@ -20,10 +18,6 @@ class BaseGeometry:
         Args:
             name (str): The name of the value.
             value (int): The value to validate.
-
-        Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than or equal to 0.
         """
         if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
