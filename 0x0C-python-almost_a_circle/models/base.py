f94c071619fb559c4d03e2eb2535987b7e95d1a3
Jonathan Kyule <121217004+JMK-WRL@users.noreply.github.com>
2023-09-18 18:54:25 +0000

    task 15
    Signed-off-by: Jonathan Kyule <121217004+JMK-WRL@users.noreply.github.com>

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Class base"""
+import json
 
 class Base:
     """Base class to manage IDs for other classes in the project."""
@@ -19,3 +20,15 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Convert a list of dictionaries to a JSON string rep.
+
+        Args:
+            list_dictionaries (list): a list of dictionaries to be converted
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
