04a89b7bbdc51471c4124d1820c651664da87cd7
Jonathan Kyule <121217004+JMK-WRL@users.noreply.github.com>
2023-09-18 18:56:59 +0000

    task 16
    Signed-off-by: Jonathan Kyule <121217004+JMK-WRL@users.noreply.github.com>

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,11 +1,9 @@
-#!/usr/bin/python3
-"""Class base"""
 import json
 
 class Base:
     """Base class to manage IDs for other classes in the project."""
 
-    __nb_objects = 0  
+    __nb_objects = 0
 
     def __init__(self, id=None):
         """
@@ -24,11 +22,31 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """
-        Convert a list of dictionaries to a JSON string rep.
+        Convert a list of dictionaries to a JSON string representation.
 
         Args:
-            list_dictionaries (list): a list of dictionaries to be converted
+            list_dictionaries (list): A list of dictionaries to be converted to JSON.
+
+        Returns:
+            str: JSON string representation of the list of dictionaries.
         """
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Save a list of instances to a JSON file.
+
+        Args:
+            list_objs (list): A list of instances to be saved to a JSON file.
+        """
+        filename = cls.__name__ + ".json"
+        data = []
+
+        if list_objs is not None:
+            data = [obj.to_dictionary() for obj in list_objs]
+
+        with open(filename, mode='w', encoding='utf-8') as file:
+            file.write(cls.to_json_string(data))
