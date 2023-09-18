4ba58b006251269f05070f4690fe7696e43f2d32
Jonathan Kyule <121217004+JMK-WRL@users.noreply.github.com>
2023-09-18 18:39:11 +0000

    task 12
    Signed-off-by: Jonathan Kyule <121217004+JMK-WRL@users.noreply.github.com>

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -38,3 +38,13 @@ class Square(Rectangle):
         """Setter for size attribute."""
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """Update attribute of the square"""
+        if args:
+            attrs = ["id", "size", "x", "y"]
+            for i, arg in enumerate(args):
+                setattr(self, attrs[i], arg)
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
