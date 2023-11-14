17bc17c4ae7ccc8d3c7991525b8edb2fc735dfa0
Jonathan <jonathankyule2@gmail.com>
2023-11-14 13:31:57 -0800

    corrections

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,26 +1,26 @@
 #!/usr/bin/node
 
 class Rectangle {
-	constructor (w, h) {
-		if (w > 0 && h > 0) {
-			this.width = w;
-			this.height = h;
-		}
-	}
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 
-	print () {
-		let x = 0;
-		let y = 0;
-		let widthPrint = '';
-		while (x < this.width) {
-			widthPrint = widthPrint + 'X';
-			x++;
-		}
-		while (y < this.height) {
-			console.log(widthPrint);
-			y++;
-		}
-	}
+  print () {
+    let x = 0;
+    let y = 0;
+    let widthPrint = '';
+    while (x < this.width) {
+      widthPrint = widthPrint + 'X';
+      x++;
+    }
+    while (y < this.height) {
+      console.log(widthPrint);
+      y++;
+    }
+  }
 }
 
 module.exports = Rectangle;
