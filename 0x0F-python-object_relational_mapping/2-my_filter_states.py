ae758f436950cba823ba769c249a4dc6a9f6291f
Jonathan <jonathankyule2@gmail.com>
2023-11-18 00:25:56 -0800

    updates task2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """
 Script that takes in an argument and displays all values in the states
 table of hbtn_0e_0_usa where name matches the argument.
@@ -8,30 +7,32 @@ table of hbtn_0e_0_usa where name matches the argument.
 import sys
 import MySQLdb
 
-if __name__ == "__main__":
+def main():
+    """
+    Displays values in the states table based on user input
+    """
     if len(sys.argv) != 5:
         print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
         sys.exit(1)
 
-
     username, password, database, state_name = sys.argv[1:]
 
     db = MySQLdb.connect(
-            host="localhost",
-            post=3306,
-            user=username,
-            passwd=password,
-            db=database
-        )
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
 
     cur = db.cursor()
-
-    cur.execute("SELECT * FROM states WHERE name=%s ORDER BY id ASC", (state_name,))
-
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (state_name,))
     rows = cur.fetchall()
-
     for row in rows:
         print(row)
-
     cur.close()
     db.close()
+
+if __name__ == "__main__":
+    main()
+
