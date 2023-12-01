0d7d57e068422ffdeef864338fa1460367496655
Jonathan <jonathankyule2@gmail.com>
2023-12-01 02:19:44 -0800

    task 8 pycodestyle

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 """
-Script that sends POST request to http://0.0.0.0:5000/search_user with a given letter.
-Usage: ./8-json_api.py <letter>
-  - The letter is sent as the value of the variable `q`.
-  - If no letter is provided, sends `q=""`.
+Script that sends POST request to http://0.0.0.0:5000/search_user.
 """
 import sys
 import requests
