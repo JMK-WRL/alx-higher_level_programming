0aeeb9b18293a8422f69ab1f42ea1be310c293d2
Jonathan <jonathankyule2@gmail.com>
2024-01-02 23:10:03 -0800

    Star wars movie title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const request = require('request');
+
+// Check if the correct number of arguments is provided
+if (process.argv.length !== 3) {
+  console.error('Usage: ./3-starwars_title.js <movie_id>');
+  process.exit(1);
+}
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+// Make a request to the Star Wars API and print the movie title
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const movieData = JSON.parse(body);
+    console.log(movieData.title);
+  }
+});
