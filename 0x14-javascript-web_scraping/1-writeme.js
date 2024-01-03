542f27e9435fba5440accbfefd6475b60b856b09
Jonathan <jonathankyule2@gmail.com>
2024-01-02 23:08:47 -0800

    status code

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+// Check if the correct number of arguments is provided
+if (process.argv.length !== 4) {
+  console.error('Usage: ./1-writeme.js <file_path> <string_to_write>');
+  process.exit(1);
+}
+
+const filePath = process.argv[2];
+const contentToWrite = process.argv[3];
+
+// Write the string content to the file in utf-8
+fs.writeFile(filePath, contentToWrite, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
